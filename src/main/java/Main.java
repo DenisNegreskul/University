41c3ffb95ce0801d45ea2f241004b0958b300f4d
Denis Negreskul <den.negreskul@gmail.com>
2022-10-23 22:30:17 +0300

    Third Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    private static final String fileName = "universityInfo.xlsx";
+    private static final String fileName = "src/main/resources/universityInfo.xlsx";
     public static void main(String[] args) {
         System.out.println(ExcelReader.readStudents(fileName));
         System.out.println(ExcelReader.readUniversities(fileName));
